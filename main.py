a59aa9e68e14c3841c95a9ede3bd1e29104044ef
TitSober <45947463+TitSober@users.noreply.github.com>
2020-04-01 19:52:03 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import playerClass
 pl = playerClass.Player(input("Enter your name: "), input("enter your gender"),input("are u bad?"), input("what race are you?: "))
+if "no" in pl.bad:
+    pl.bad = False
+elif "yes" in pl.bad:
+    pl.bad = True
+
+print(pl.bad)
+
