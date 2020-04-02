366ea9c1ab88902c9a84b64df82ce3f4309ee4e8
TitSober <45947463+TitSober@users.noreply.github.com>
2020-04-02 20:00:30 +0200

    Add files via upload

--- playerClass.py
+++ playerClass.py
@@ -1,11 +1,23 @@
-#ban is bool does nothing now
+#bad is bool does nothing now
 
 #gender not important rn
 class Player:
-    def __init__(self, name, gender, bad, race):
+    def __init__(self, name, gender, bad, race, hp):
         self.name = name
         self.gender = gender
         self.bad = bad
         self.race = race
+        self.hp = hp
+
     def introduction(self):
         print("hi my name is", self.name," I am ",self.race)
+
+
+class enemy1:
+    def __init__(self,hp,attack,speed):
+        self.hp = hp
+        self.attack = attack
+        self.speed = speed
+    def stats(self):
+        print("This enemy has: ",self.hp," health, ",self.attack, " attack power and ",self.speed, "speed!")
+
